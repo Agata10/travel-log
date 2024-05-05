4c5d8cc871b3f4c828634b8250913edd0476a6e2
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-05 13:05:33 -0600

    Refactor routes, add user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,48 @@
+const User = require('../models/userModel');
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const result = await User.find();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const result = await User.create(req.body);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const result = await User.findById(req.params.id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const result = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const result = await User.findByIdAndDelete(req.params.id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,52 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel');
+const userController = require('../controllers/userController');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await User.find();
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const result = await User.create(req.body);
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    const result = await User.findById(req.params.id);
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const result = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const result = await User.findByIdAndDelete(req.params.id);
-    res.json(result);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get('/', userController.getAllUsers);
+router.post('/', userController.createUser);
+router.get('/:id', userController.getUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
