485eaeb446496abf2d6188d9989fb30812b84cd8
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-05 12:09:06 -0600

    Setup error handling and logger in express server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,27 @@ require('dotenv').config();
 
 //setup the express app
 const epxress = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = epxress();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.json({ success: 'YAY' });
+//logger
+app.use(morgan('dev'));
+
+//allow access to body sent in the request
+app.use(epxress.json());
+
+app.get('/', (req, res, next) => {
+  res.json({ msg: 'YAY' }).status(200);
+});
+
+//error handling
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const msg = err.message;
+  res.status(status);
+  res.json({ error: msg, status: status });
 });
 
 app.listen(PORT, () => {
