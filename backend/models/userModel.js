6ece4914ecca012cee8c89b12eb651cf3db64610
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-09 19:33:26 -0600

    Add more validation for user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, 'First name required'],
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, 'Last name required'],
     },
     email: {
       type: String,
       required: true,
+      unique: [true, 'Email required'],
+      validate: [isEmail, 'Please enter a valid email'],
     },
     password: {
       type: String,
-      required: true,
+      required: [true, 'Password required'],
+      minLength: [8, 'Minimum password length is 8 characters'],
     },
   },
   { timestamps: true }
