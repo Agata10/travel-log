aa7a9b7193c54b2515aee64345a812a837475bbb
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-09 19:55:05 -0600

    Fix bug with the get route for user trip

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Trip = require('../models/tripModel');
 
 //get all user trips
-router.get('/userId', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
     const userId = req.params.userId;
     const trips = await Trip.findById(userId);
@@ -19,6 +19,16 @@ router.post('/', async (req, res, next) => {
     const trip = await Trip.create(req.body);
     res.json(trip);
   } catch (err) {
+    next(err);
+  }
+});
+
+//update single trip
+router.get('/:id', async (req, res, next) => {
+  try {
+    const trip = await Trip.findByIdAndUpdate(req.params.id, { new: true });
+    res.json(trip);
+  } catch {
     next(err);
   }
 });
