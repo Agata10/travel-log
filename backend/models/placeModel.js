a87b2251d07e50b8867aee1023c15cf88173a73a
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-11 15:09:56 -0600

    Add place model

---
+++ backend/models/placeModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema(
+  {
+    tripId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Trip',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    date: Date,
+    img: String,
+    description: String,
+    favourite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Place', placeSchema);
