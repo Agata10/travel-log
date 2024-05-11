92e31b936c803bdb9fe1313211806ded1d8808e6
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-11 17:55:50 -0600

    Check if user already exists in sign up funtion

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,10 @@ const User = require('../models/userModel');
 
 module.exports.signup = async (req, res, next) => {
   try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
     const result = await User.create({ ...req.body });
     res.json(result);
   } catch (err) {
