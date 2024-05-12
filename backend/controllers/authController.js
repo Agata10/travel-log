4772b668734f8ce3b290b3334fcd06d18d06b1ed
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-12 16:29:31 -0600

    Add genereting token for sign up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,20 +1,43 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const genereteToken = (_id) => {
-  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+  const payload = { id: _id };
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '3d' });
 };
 
 module.exports.signup = async (req, res, next) => {
   try {
-    const userExists = await User.findOne({ email: req.body.email });
-    if (userExists) {
+    const userIsFound = await User.findOne({ email: req.body.email });
+    if (userIsFound) {
       return res.status(400).json({ error: 'User already exists' });
     }
     const user = await User.create(req.body);
     //create token
     const token = genereteToken(user._id);
     res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const userIsFound = await User.findOne({ email: req.body.email });
+    if (!userIsFound) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+    const isPassValid = await bcrypt.compare(
+      req.body.password,
+      userIsFound.password
+    );
+    if (!isPassValid) {
+      return res.status(400).json({ error: 'Password does not match' });
+    }
+    //create token
+    const token = genereteToken(userIsFound._id);
+    res.json({ email: userIsFound.email, token });
   } catch (err) {
     next(err);
   }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
-router.post('/login', (req, res, next) => {
-  res.json({ login: 'logged in' });
-});
+router.post('/login', authController.login);
 
 module.exports = router;
