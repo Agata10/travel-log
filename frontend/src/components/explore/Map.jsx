f90520efa737bdd08a2ffc7ac571fe3d18910bfc
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-25 16:27:47 -0600

    Add user loaction to the map

--- frontend/src/components/explore/Map.jsx
+++ frontend/src/components/explore/Map.jsx
@@ -1,42 +1,53 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useMap, useMapEvents } from 'react-leaflet/hooks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
 
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
-
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>You are here</Popup>
-    </Marker>
-  );
-}
-
 const Map = () => {
+  const [position, setPosition] = useState([51.505, -0.09]); //lat, lng
+  const [isLoading, setIsLoading] = useState(true);
+
+  //get user location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setPosition([position.coords.latitude, position.coords.longitude]);
+        setIsLoading(false);
+      },
+      () => {
+        setIsLoading(false);
+        console.log('Error, no position avaiable');
+      }
+    );
+  }, []);
+  {
+    console.log(position);
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>; // Render a loading indicator
+  }
+
   return (
     <div className="w-full h-screen">
-      <MapContainer
-        center={{ lat: 51.505, lng: -0.09 }}
-        zoom={13}
-        scrollWheelZoom={false}
-        className="w-full h-full"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <LocationMarker />
-      </MapContainer>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <MapContainer
+          center={position}
+          zoom={13}
+          scrollWheelZoom={false}
+          className="w-full h-full"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>You are here</Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </div>
   );
 };
