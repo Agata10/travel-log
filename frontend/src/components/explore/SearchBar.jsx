f4c1935cf8a2ae1e1dc437687ce5ebc4550eb2aa
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-26 16:19:24 -0600

    Add searchbar functionality

--- frontend/src/components/explore/SearchBar.jsx
+++ frontend/src/components/explore/SearchBar.jsx
@@ -1,5 +1,74 @@
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { useEffect, useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment } from '@mui/material';
+
 const SearchBar = ({ position, setPosition, setIsLoading }) => {
-  return <div className="w-10/12"></div>;
+  const [options, setOptions] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [selectedPlace, setSelectedPlace] = useState(null);
+
+  useEffect(() => {
+    if (selectedPlace) {
+      setPosition({
+        lat: selectedPlace?.properties.lat,
+        lng: selectedPlace?.properties.lon,
+      });
+    }
+  }, [selectedPlace]);
+
+  const handleInputChange = async (event, newValue) => {
+    setInputValue(newValue);
+
+    if (newValue.length > 3) {
+      // Call Geoapify API to get autocomplete suggestions
+      const response = await fetch(
+        `https://api.geoapify.com/v1/geocode/autocomplete?text=${newValue}&apiKey=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      const data = await response.json();
+      setOptions(data.features);
+    }
+  };
+
+  const handleSelectedOption = (event, value) => {
+    setSelectedPlace(value);
+
+    console.log(value);
+  };
+  return (
+    <div className="w-10/12">
+      <Autocomplete
+        value={selectedPlace}
+        onChange={handleSelectedOption}
+        inputValue={inputValue}
+        onInputChange={handleInputChange}
+        isOptionEqualToValue={(option, value) =>
+          option.properties.formatted === value.properties?.formatted
+        }
+        options={options}
+        sx={{ width: 300 }}
+        getOptionLabel={(option) => option.properties.formatted}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Explore new places..."
+            variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        )}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
