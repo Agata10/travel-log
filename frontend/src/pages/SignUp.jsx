2c5cf3876184d1dc56b63aa878099ad14d36dcc5
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-06-08 12:46:11 -0600

    Add validation and styling to the sig up form

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,13 @@
-import { Box, Container, Typography, useTheme } from '@mui/material';
-import { Button, TextField } from '@mui/material';
-import { useContext, useState } from 'react';
+import {
+  Avatar,
+  Box,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import { Button } from '@mui/material';
+import { useContext, useRef, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
@@ -23,23 +27,17 @@ const formData = {
   password: '',
 };
 
-const errors = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  password: '',
-};
-
 const SignUp = () => {
   const theme = useTheme();
   const navigate = useNavigate();
   const [form, setForm] = useState(formData);
-  const [isError, setIsError] = useState(false);
-  const [errorText, setErrorText] = useState(errors);
+  const confpassInput = useRef();
+  const [error, setError] = useState(false);
   const [serverError, setServerError] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const authContext = useContext(AuthContext);
   const { setAuthUser } = authContext;
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up('md'));
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -88,19 +86,39 @@ const SignUp = () => {
     setForm((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handleChangeConfPass = () => {
+    if (confpassInput.current.value !== form.password) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <>
-      <Container
+      <Box
         sx={{
           display: 'flex',
           flexDirection: 'column',
           width: '100%',
-          height: '90vh',
+          height: '100vh',
           alignItems: ' center',
           paddingTop: 4,
+          overflow: 'auto',
+          marginBottom: '80px',
         }}
       >
-        <Typography variant="h6" gutterBottom>
+        <Avatar
+          sx={{
+            color: theme.palette.primary.dark,
+            backgroundColor: theme.palette.primary.light,
+          }}
+        />
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{ fontWeight: 500, color: theme.palette.primary.dark }}
+        >
           Sign up
         </Typography>
         <ValidatorForm
@@ -108,27 +126,24 @@ const SignUp = () => {
           onSubmit={handleSubmit}
           noValidate
           autoComplete="off"
-          sx={{
+          style={{
+            width: isSmallScreen ? '30%' : '60%',
             display: 'flex',
             flexDirection: 'column',
-            width: '30%',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom: '20px',
           }}
         >
           <TextValidator
             label="First name"
             variant="outlined"
             type="text"
-            sx={inputStyle}
+            fullWidth
+            sx={{ ...inputStyle, margin: '20px auto' }}
             validators={['required', 'minStringLength:3']}
             errorMessages={[
-              'This field is required',
-              'Minimum of 3 characters',
+              'This field is required.',
+              'Minimum of 3 characters.',
             ]}
             required
-            fullWidth
             name="firstName"
             value={form.firstName}
             onChange={handleChange}
@@ -137,13 +152,14 @@ const SignUp = () => {
             label="Last name"
             variant="outlined"
             type="text"
-            sx={inputStyle}
+            sx={{ ...inputStyle, marginBottom: '20px' }}
             validators={['required', 'minStringLength:3']}
             errorMessages={[
-              'This field is required',
-              'Minimum of 3 characters',
+              'This field is required.',
+              'Minimum of 3 characters.',
             ]}
             required
+            fullWidth
             name="lastName"
             value={form.lastName}
             onChange={handleChange}
@@ -152,93 +168,126 @@ const SignUp = () => {
             label="Email"
             variant="outlined"
             type="email"
-            sx={inputStyle}
+            fullWidth
+            sx={{ ...inputStyle, marginBottom: '20px' }}
             validators={['required', 'isEmail']}
-            errorMessages={['This field is required', 'Email is not valid']}
+            errorMessages={['This field is required.', 'Email is not valid.']}
             required
             name="email"
             value={form.email}
             onChange={handleChange}
           />
-          <FormControl
-            sx={{ ...inputStyle, m: 1, width: '25ch' }}
-            variant="outlined"
-            required
+          <Box
+            sx={{
+              display: 'flex',
+              gap: { xs: '20px', sm: 1 },
+              justifyContent: 'center',
+              flexDirection: { xs: 'column', sm: 'row' },
+              width: '100%',
+            }}
           >
-            <TextValidator
-              id="password"
-              name="password"
-              type={showPassword ? 'text' : 'password'}
-              value={form.password}
-              onChange={handleChange}
-              validators={[
-                'required',
-                'matchRegexp:^(?=.*\\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$',
-              ]}
-              errorMessages={[
-                'This field is required',
-                'At least 8 characters' +
-                  '\n' +
-                  'One symbol \n One number \n One upper letter \n One lower letter',
-              ]}
-              label="Password"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                      sx={{ color: theme.palette.primary.main }}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-          <FormControl
-            sx={{ ...inputStyle, m: 1, width: '25ch' }}
-            variant="outlined"
-            required
-          >
-            <TextValidator
-              id="conf-password"
-              type={showPassword ? 'text' : 'password'}
-              validators={['required', 'isEmail']}
-              errorMessages={['This field is required', 'Email is not valid']}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                      sx={{ color: theme.palette.primary.main }}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              label="Confirm Password"
-            />
-          </FormControl>
-          <div className="pt-0 pb-4 text-sm text-red-800 font-semibold">
+            <FormControl
+              sx={{ ...inputStyle, width: { xs: '100%', sm: '50%' } }}
+              variant="outlined"
+              required
+            >
+              <TextValidator
+                id="password"
+                sx={{
+                  ...inputStyle,
+                  width: '100%',
+                  '& .MuiFormHelperText-root': {
+                    whiteSpace: 'pre-line',
+                    paddingTop: 1,
+                    paddingLeft: 1,
+                    margin: 0,
+                    lineHeight: '14px',
+                  },
+                }}
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                value={form.password}
+                onChange={handleChange}
+                validators={[
+                  'required',
+                  'matchRegexp:^(?=.*\\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$',
+                ]}
+                errorMessages={[
+                  'This field is required.',
+                  'Please provide password with: \n At least 8 characters. \n One symbol. \n One number. \n One upper letter. \n One lower letter.',
+                ]}
+                label="Password"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                        sx={{ color: theme.palette.primary.main }}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+            <FormControl
+              sx={{ ...inputStyle, width: { xs: '100%', sm: '50%' } }}
+              variant="outlined"
+              required
+            >
+              <TextValidator
+                sx={{
+                  ...inputStyle,
+                  width: '100%',
+                }}
+                onChange={handleChangeConfPass}
+                id="conf-password"
+                inputRef={confpassInput}
+                type={showPassword ? 'text' : 'password'}
+                error={error}
+                helperText={error ? "Passwords don't match." : ''}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                        sx={{ color: theme.palette.primary.main }}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                label="Confirm Password"
+              />
+            </FormControl>
+          </Box>
+          <div className="pt-0 pb-4 text-sm text-red-800 font-semibold text-center">
             {serverError === 'User already exists' ? serverError : ''}
           </div>
-          <Button
-            type="submit"
-            variant="contained"
-            size="medium"
-            sx={{ fontSize: theme.typography.body1, fontWeight: 500 }}
-            endIcon={<KeyboardArrowRightIcon />}
-          >
-            Submit
-          </Button>
+          <Box sx={{ display: 'flex' }}>
+            <Button
+              type="submit"
+              variant="contained"
+              size="medium"
+              sx={{
+                fontSize: theme.typography.body1,
+                fontWeight: 500,
+                width: { xs: '50%', sm: '25%' },
+                margin: '10px auto',
+              }}
+              endIcon={<KeyboardArrowRightIcon />}
+            >
+              Submit
+            </Button>
+          </Box>
         </ValidatorForm>
-      </Container>
+      </Box>
       <Footer />
     </>
   );
