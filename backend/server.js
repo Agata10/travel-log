83610acb3d59522dc5c9f873fc2a2bb4fe44db02
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-06-10 13:30:30 -0600

    Change port if 8080 not accessible

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 //connect to database
 const configDB = require('./conn');
